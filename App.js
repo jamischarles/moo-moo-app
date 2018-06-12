5b690fa1b500df72d0350a2cf061f44ffbfacc2f
tmat06 <timmy.a.matthews@gmail.com>
2018-06-12 10:43:37 -0600

    Map is now it's own component, prettified

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { StyleSheet, Button, View, Text, TextInput } from 'react-native';
-import { Marker } from 'react-native-maps';
 import { createMaterialTopTabNavigator } from 'react-navigation';
 import { MapView } from 'expo';
 import { withFormik } from 'formik';
-import Geocoder from 'react-native-geocoding';
-import { GOOGLE_API } from 'react-native-dotenv'
+import Map from './components/Map';
 
-Geocoder.init(GOOGLE_API);
 
 class HomeScreen extends React.Component {
   render() {
@@ -73,10 +70,8 @@ class FormScreen extends React.Component {
 
         <Button
           title="Create Order"
-          // onPress={() => this.props.navigation.navigate('LocationPicker')}
           onPress={() => this.props.navigation.navigate('Order')}
         />
-
         {/* <Button onPress={this.submitFormViaEmail} title="Place Order" /> */}
       </View>
     );
@@ -84,7 +79,6 @@ class FormScreen extends React.Component {
 }
 
 var wrapForm = withFormik({});
-
 
 //Second Order Form as requested by Kenfo
 
@@ -138,17 +132,12 @@ class Order extends React.Component {
           title="Choose delivery location"
           onPress={() => this.props.navigation.navigate('LocationPicker')}
         />
-
       </View>
     );
   }
 }
 
 var wrapForm = withFormik({});
-
-
-
-
 
 class Confirm extends React.Component {
   render() {
@@ -158,106 +147,6 @@ class Confirm extends React.Component {
           Thank you for your order. We will call you at 123-123-1222 within 5
           minutes to confirm your order.
         </Text>
-      </View>
-    );
-  }
-}
-
-class Map extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      currentRegionName: '',
-      region: {
-        latitude: 11.5683972886659,
-        longitude: 104.92227526802127,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      },
-      initialRegion: {
-        latitude: 11.5683972886659,
-        longitude: 104.92227526802127,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      },
-    }
-  }
-
-
-
-  onRegionChange(region) {
-
-    this.setState({ region }, () => {
-      setTimeout(() => {
-        
-        Geocoder.from(region.latitude, region.longitude)
-          .then(json => {
-            var addressComponent = json.results[0].address_components[0];
-            this.setState({ currentRegionName: addressComponent.long_name });
-          })
-          .catch(error => console.warn(error));
-      }, 500);})
-  }
-
-  resetRegion() {
-    this.setState({
-      region: this.state.initialRegion
-    }, () => {
-      Geocoder.from(this.state.initialRegion.latitude, this.state.initialRegion.longitude)
-        .then(json => {
-          var addressComponent = json.results[0].address_components[0];
-          this.setState({ currentRegionName: addressComponent.long_name });
-        })
-        .catch(error => console.warn(error));
-    })
-  }
-
-  confirmCoordinates(){
-
-  }
-
-  render() {
-    return (
-      <View
-        style={{
-          position: 'absolute',
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0,
-        }}
-      >
-        <MapView
-          style={{
-            flex: 3
-          }}
-          region={this.state.region}
-          onRegionChange={(region) => this.onRegionChange(region)}
-        >
-          <Marker
-            coordinate={{ latitude: this.state.region.latitude, longitude: this.state.region.longitude }}
-          />
-          <Marker
-            coordinate={{ latitude: 11.568470353160166, longitude: 104.95745898407355 }}
-          />
-
-        </MapView>
-        <View style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-
-          <Text >
-            `{this.state.currentRegionName}, Phnom Penh, Cambodia`
-          </Text>
-          <Button
-            title="Reset Location"
-            onPress={() => this.resetRegion()}
-            style={{ flex: 1 }}
-          />
-          <Button
-            style={{ flex: 1 }}
-            title="Confirm"
-            onPress={() => this.props.navigation.navigate('Confirm')}
-          />
-        </View>
       </View>
     );
   }
