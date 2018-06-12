ed284a4d8c707482f5c4dc1e551f415e386da3a2
tmat06 <timmy.a.matthews@gmail.com>
2018-06-12 01:39:16 -0600

    changed stackNavigator to tabNavigator/map has pins and reset button as well as street name appearing/Form is split between Form and Order screen

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import {StyleSheet, Button, View, Text, TextInput} from 'react-native';
-import {createStackNavigator} from 'react-navigation';
-import {MapView} from 'expo';
-import {withFormik} from 'formik';
+import { StyleSheet, Button, View, Text, TextInput } from 'react-native';
+import { Marker } from 'react-native-maps';
+import { createMaterialTopTabNavigator } from 'react-navigation';
+import { MapView } from 'expo';
+import { withFormik } from 'formik';
+import Geocoder from 'react-native-geocoding';
+import { GOOGLE_API } from 'react-native-dotenv'
+
+Geocoder.init(GOOGLE_API);
 
 class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Welcome to Moo Moo farms.</Text>
         <Text>You will love our milk. </Text>
         <Button
@@ -42,8 +47,8 @@ class FormScreen extends React.Component {
       <View>
         <Text>What is your name? </Text>
         <TextInput
-          onChangeText={text => props.setFieldValue('email', text)}
-          value={props.values.email}
+          onChangeText={name => props.setFieldValue('name', name)}
+          value={props.values.name}
           style={{
             width: 200,
             height: 44,
@@ -52,10 +57,74 @@ class FormScreen extends React.Component {
             borderColor: '#ccc',
           }}
         />
-        <Text>Phone number ? </Text>
+        <Text>Phone number? </Text>
         <TextInput
-          onChangeText={text => props.setFieldValue('email', text)}
-          value={props.values.email}
+          onChangeText={phone => props.setFieldValue('phone', phone)}
+          value={props.values.phone}
+          style={{
+            width: 200,
+            height: 44,
+            padding: 8,
+            borderWidth: 1,
+            borderColor: '#ccc',
+          }}
+        />
+
+
+        <Button
+          title="Create Order"
+          // onPress={() => this.props.navigation.navigate('LocationPicker')}
+          onPress={() => this.props.navigation.navigate('Order')}
+        />
+
+        {/* <Button onPress={this.submitFormViaEmail} title="Place Order" /> */}
+      </View>
+    );
+  }
+}
+
+var wrapForm = withFormik({});
+
+
+//Second Order Form as requested by Kenfo
+
+class Order extends React.Component {
+  constructor() {
+    super();
+    this.submitFormViaEmail = this.submitFormViaEmail.bind(this);
+  }
+  submitFormViaEmail() {
+    var url =
+      'https://hooks.zapier.com/hooks/catch/3120953/an5a96?name=lamis&city=san jose';
+    // var url = 'https://jsonplaceholder.typicode.com/posts/1'; // fake
+
+    fetch(url, {})
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        this.props.navigation.navigate('Confirm');
+      });
+  }
+  render() {
+    var props = this.props;
+    return (
+      <View>
+        <Text>Bottle Size? </Text>
+        <TextInput
+          onChangeText={bottleSize => props.setFieldValue('bottleSize', bottleSize)}
+          value={props.values.bottleSize}
+          style={{
+            width: 200,
+            height: 44,
+            padding: 8,
+            borderWidth: 1,
+            borderColor: '#ccc',
+          }}
+        />
+        <Text>Quantity?</Text>
+        <TextInput
+          onChangeText={quantity => props.setFieldValue('quantity', quantity)}
+          value={props.values.quantity}
           style={{
             width: 200,
             height: 44,
@@ -66,11 +135,10 @@ class FormScreen extends React.Component {
         />
 
         <Button
-          title="Choose deliviery location "
+          title="Choose delivery location"
           onPress={() => this.props.navigation.navigate('LocationPicker')}
         />
 
-        <Button onPress={this.submitFormViaEmail} title="Place Order" />
       </View>
     );
   }
@@ -78,12 +146,16 @@ class FormScreen extends React.Component {
 
 var wrapForm = withFormik({});
 
-class SuccessScreen extends React.Component {
+
+
+
+
+class Confirm extends React.Component {
   render() {
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>
-          Thank you for your order. We will call you at 123-123-1222 witin 5
+          Thank you for your order. We will call you at 123-123-1222 within 5
           minutes to confirm your order.
         </Text>
       </View>
@@ -92,19 +164,101 @@ class SuccessScreen extends React.Component {
 }
 
 class Map extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      currentRegionName: '',
+      region: {
+        latitude: 11.5683972886659,
+        longitude: 104.92227526802127,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+      initialRegion: {
+        latitude: 11.5683972886659,
+        longitude: 104.92227526802127,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    }
+  }
+
+
+
+  onRegionChange(region) {
+
+    this.setState({ region }, () => {
+      setTimeout(() => {
+        
+        Geocoder.from(region.latitude, region.longitude)
+          .then(json => {
+            var addressComponent = json.results[0].address_components[0];
+            this.setState({ currentRegionName: addressComponent.long_name });
+          })
+          .catch(error => console.warn(error));
+      }, 500);})
+  }
+
+  resetRegion() {
+    this.setState({
+      region: this.state.initialRegion
+    }, () => {
+      Geocoder.from(this.state.initialRegion.latitude, this.state.initialRegion.longitude)
+        .then(json => {
+          var addressComponent = json.results[0].address_components[0];
+          this.setState({ currentRegionName: addressComponent.long_name });
+        })
+        .catch(error => console.warn(error));
+    })
+  }
+
+  confirmCoordinates(){
+
+  }
+
   render() {
     return (
-      <MapView
+      <View
         style={{
-          flex: 1,
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0,
         }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+      >
+        <MapView
+          style={{
+            flex: 3
+          }}
+          region={this.state.region}
+          onRegionChange={(region) => this.onRegionChange(region)}
+        >
+          <Marker
+            coordinate={{ latitude: this.state.region.latitude, longitude: this.state.region.longitude }}
+          />
+          <Marker
+            coordinate={{ latitude: 11.568470353160166, longitude: 104.95745898407355 }}
+          />
+
+        </MapView>
+        <View style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+
+          <Text >
+            `{this.state.currentRegionName}, Phnom Penh, Cambodia`
+          </Text>
+          <Button
+            title="Reset Location"
+            onPress={() => this.resetRegion()}
+            style={{ flex: 1 }}
+          />
+          <Button
+            style={{ flex: 1 }}
+            title="Confirm"
+            onPress={() => this.props.navigation.navigate('Confirm')}
+          />
+        </View>
+      </View>
     );
   }
 }
@@ -118,12 +272,13 @@ const styles = StyleSheet.create({
   },
 });
 
-export default createStackNavigator(
+export default createMaterialTopTabNavigator(
   {
     Home: HomeScreen,
-    LocationPicker: Map,
     Form: wrapForm(FormScreen),
-    Success: SuccessScreen,
+    Order: wrapForm(Order),
+    LocationPicker: Map,
+    Confirm: Confirm,
   },
   {
     initialRouteName: 'Home',
