78ff88efc040be2787f4b1272d1d2455b98774b1
Tim Matthews <timmy.a.matthews@gmail.com>
2018-06-15 00:07:40 -0600

    Tim styling (#2)
    * nested routing, styled landing page and forms, few changes with forms that will be dealt with tomorrow

--- App.js
+++ App.js
@@ -1,40 +1,88 @@
 import React from 'react';
-import { StyleSheet, Button, View, Text, TextInput } from 'react-native';
-import { createMaterialTopTabNavigator } from 'react-navigation';
+import { StyleSheet, Button, View, Text, TextInput, ImageBackground, Picker } from 'react-native';
+import { createStackNavigator, createMaterialTopTabNavigator } from 'react-navigation';
 import { MapView } from 'expo';
 import { withFormik } from 'formik';
 import Map from './components/Map';
+import { Icon } from 'react-native-elements';
 
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <View style={{ flex: 3, backgroundColor: 'grey' }}>
-          <Text>Welcome to Moo Moo farms.</Text>
-        </View>
-        <View style={{ flex: 2, backgroundColor: 'purple' }}>
-          <Text>You will love our milk. </Text>
-        </View>
-        <View style={{ flex: 1 }}>
+        <ImageBackground style={styles.carousel}
+          source={{ uri: 'https://www.maxpixel.net/static/photo/1x/Cow-White-Black-Cows-Pasture-Nature-2306534.jpg' }}
+        >
+          <View style={styles.carouselContainer}>
+            <Text style={{ fontFamily: 'Georgia-Bold', color: 'white' }}>Welcome to Moo Moo farms.</Text>
+
+          </View>
+        </ImageBackground>
+        <View style={styles.startMilkOrderContainer}>
           <Button
+            color="white"
             title="Start Milk order"
             onPress={() => this.props.navigation.navigate('Form')}
           />
         </View>
         <View style={{ flex: 2, flexDirection: 'row', justifyContent: 'space-evenly' }}>
-          <Text style={{ flex: 1, backgroundColor: 'green' }}>
-            Dog
-          </Text>
-          <Text style={{ flex: 1, backgroundColor: 'yellow' }}>
-            Dog
-          </Text>
-          <Text style={{ flex: 1, backgroundColor: 'blue' }}>
-            Dog
-          </Text>
-          <Text style={{ flex: 1, backgroundColor: 'red' }}>
-            Dog
-          </Text>
+          <View style={styles.infoBox}>
+            <Icon
+              size={40}
+              style={{ flex: 1 }}
+              color={'white'}
+              name='emoji-happy'
+              type='entypo'
+            />
+            <Text
+              style={{ color: 'white', flex: 2 }}
+            >
+              This Milk is Fantastic
+            </Text>
+          </View>
+          <View style={styles.infoBox}>
+            <Icon
+              size={40}
+              style={{ flex: 1 }}
+              color={'white'}
+              name='location-pin'
+              type='entypo'
+            />
+            <Text
+              style={{ color: 'white', flex: 2 }}
+            >
+              This Milk is Fantastic
+            </Text>
+          </View>
+          <View style={styles.infoBox}>
+            <Icon
+              size={40}
+              style={{ flex: 1 }}
+              color={'white'}
+              name='thumb-up'
+              type='SimpleLineIcons'
+            />
+            <Text
+              style={{ color: 'white', flex: 2 }}
+            >
+              This Milk is Fantastic
+            </Text>
+          </View>
+          <View style={styles.infoBox}>
+            <Icon
+              size={40}
+              style={{ flex: 1 }}
+              color={'white'}
+              name='explore'
+              type='MaterialCommunityIcons'
+            />
+            <Text
+              style={{ color: 'white', flex: 2 }}
+            >
+              This Milk is Fantastic
+            </Text>
+          </View>
         </View>
       </View>
     );
@@ -44,27 +92,33 @@ class HomeScreen extends React.Component {
 class FormScreen extends React.Component {
   constructor() {
     super();
-    this.submitFormViaEmail = this.submitFormViaEmail.bind(this);
+    this.state = {
+      name: '',
+      phone: ''
+    }
+    // this.submitFormViaEmail = this.submitFormViaEmail.bind(this);
   }
-  submitFormViaEmail() {
-    var url =
-      'https://hooks.zapier.com/hooks/catch/3120953/an5a96?name=lamis&city=san jose';
-    // var url = 'https://jsonplaceholder.typicode.com/posts/1'; // fake
+  // submitFormViaEmail() {
+  //   var url =
+  //     'https://hooks.zapier.com/hooks/catch/3120953/an5a96?name=lamis&city=san jose';
+  //   // var url = 'https://jsonplaceholder.typicode.com/posts/1'; // fake
 
-    fetch(url, {})
-      .then(response => response.json())
-      .then(json => {
-        console.log(json);
-        this.props.navigation.navigate('Success');
-      });
-  }
+  //   fetch(url, {})
+  //     .then(response => response.json())
+  //     .then(json => {
+  //       console.log(json);
+  //       this.props.navigation.navigate('Success');
+  //     });
+  // }
   render() {
     var props = this.props;
     return (
-      <View>
-        <Text>What is your name? </Text>
+      <View style={styles.form}>
+
+        <Text style={{ color: 'white', fontFamily: 'Georgia-Bold', fontSize: 25 }}>What is your name? </Text>
         <TextInput
-          onChangeText={name => props.setFieldValue('name', name)}
+          // onChangeText={name => props.setFieldValue('name', name)}
+          onChangeText={name => this.setState({ name })}
           value={props.values.name}
           style={{
             width: 200,
@@ -74,9 +128,11 @@ class FormScreen extends React.Component {
             borderColor: '#ccc',
           }}
         />
-        <Text>Phone number? </Text>
+
+        <Text style={{ color: 'white', fontFamily: 'Georgia-Bold', fontSize: 25 }}>Phone number? </Text>
         <TextInput
-          onChangeText={phone => props.setFieldValue('phone', phone)}
+          // onChangeText={phone => props.setFieldValue('phone', phone)}
+          onChangeText={phone => this.setState({ phone })}
           value={props.values.phone}
           style={{
             width: 200,
@@ -89,6 +145,8 @@ class FormScreen extends React.Component {
 
 
         <Button
+          style={{ flex: 1 }}
+          color={'white'}
           title="Create Order"
           onPress={() => this.props.navigation.navigate('Order')}
         />
@@ -105,6 +163,19 @@ var wrapForm = withFormik({});
 class Order extends React.Component {
   constructor() {
     super();
+    this.state = {
+      bottleSize: '',
+      sizes: [
+        {
+          label: '1 Liter',
+          value: '1 Liter'
+        },
+        {
+          label: '2 Liter',
+          value: '2 Liter'
+        },
+      ]
+    }
     this.submitFormViaEmail = this.submitFormViaEmail.bind(this);
   }
   submitFormViaEmail() {
@@ -122,20 +193,23 @@ class Order extends React.Component {
   render() {
     var props = this.props;
     return (
-      <View>
-        <Text>Bottle Size? </Text>
-        <TextInput
-          onChangeText={bottleSize => props.setFieldValue('bottleSize', bottleSize)}
-          value={props.values.bottleSize}
-          style={{
-            width: 200,
-            height: 44,
-            padding: 8,
-            borderWidth: 1,
-            borderColor: '#ccc',
-          }}
-        />
-        <Text>Quantity?</Text>
+      <View style={styles.form}>
+        <Text style={{ color: 'white' }}>Bottle Size? </Text>
+        <View style={{ flexDirection: 'row' }}>
+          {/* Would like to use a switch here or some other option */}
+          <Button
+            style={{ flex: 1 }}
+            title="1 Liter"
+            onPress={() => this.setState({ bottleSize: '1 Liter' })}
+          />
+          <Button
+            style={{ flex: 1 }}
+            title="2 Liter"
+            onPress={() => this.setState({ bottleSize: '2 Liter' })}
+          />
+        </View>
+
+        <Text style={{ color: 'white' }}>Quantity?</Text>
         <TextInput
           onChangeText={quantity => props.setFieldValue('quantity', quantity)}
           value={props.values.quantity}
@@ -160,6 +234,39 @@ class Order extends React.Component {
 var wrapForm = withFormik({});
 
 class Confirm extends React.Component {
+  constructor() {
+    super()
+    this.submitFormViaEmail = this.submitFormViaEmail.bind(this);
+  }
+
+  submitFormViaEmail() {
+    var url =
+      'https://hooks.zapier.com/hooks/catch/3120953/an5a96?name=lamis&city=san jose';
+    // var url = 'https://jsonplaceholder.typicode.com/posts/1'; // fake
+
+    fetch(url, {})
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        this.props.navigation.navigate('Success');
+      });
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>
+          Is this right?
+        </Text>
+        <Button
+          title='confirm'
+          onPress={() => this.props.navigation.navigate('Success')}
+        />
+      </View>
+    );
+  }
+}
+class Success extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -179,20 +286,103 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  carousel: {
+    flex: 4,
+    width: '100%',
+    backgroundColor: 'black',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  carouselContainer: {
+    backgroundColor: '#272727',
+    padding: '5%',
+    borderRadius: 25
+  },
+  title: {
+    color: 'white',
+  },
+  startMilkOrderContainer: {
+    width: '100%',
+    backgroundColor: '#272727',
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  infoBox: {
+    flex: 1,
+    backgroundColor: 'black',
+    paddingTop: 20,
+    borderWidth: 1,
+    borderColor: '#272727',
+    padding: 10
+  },
+  form: {
+    flex: 1,
+    backgroundColor: 'black',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+  }
 });
 
-export default createMaterialTopTabNavigator(
+export default createStackNavigator(
   {
     Home: HomeScreen,
-    Form: wrapForm(FormScreen),
-    Order: wrapForm(Order),
-    LocationPicker: Map,
-    Confirm: Confirm,
+    Form: createMaterialTopTabNavigator(
+      {
+        Form: wrapForm(FormScreen),
+        Order: wrapForm(Order),
+        LocationPicker: Map,
+        Confirm: Confirm,
+      },
+      {
+        initialRouteName: 'Form',
+        tabBarOptions: {
+          style: {
+            backgroundColor: 'black'
+          },
+          tabStyle: {
+            backgroundColor: '#272727'
+          },
+          labelStyle: {
+            color: 'white'
+          }
+        }
+      },
+    ),
+    Success: Success,
+
   },
   {
-    initialRouteName: 'Home',
+    initialRoute: 'Home',
+    headerMode: 'none',
+    navigationOptions: {
+      headerVisible: false,
+    }
   },
-);
+
+)
+
+// export default createMaterialTopTabNavigator(
+//   {
+//     Home: HomeScreen,
+//     Form: wrapForm(FormScreen),
+//     Order: wrapForm(Order),
+//     LocationPicker: Map,
+//     Confirm: Confirm,
+//   },
+//   {
+//     initialRouteName: 'Home',
+//   },
+// );
 
 // helpful for more control
 // export default class App extends React.Component {
